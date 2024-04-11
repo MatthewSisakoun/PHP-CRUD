8e32b6f811aa7e246a8088b5fbd3d7bd6914e676
MatthewSisakoun <matthew.sisakoun@gmail.com>
2024-04-11 05:39:22 +0200

    seulement le formulaire

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>CRUD Matthew Sisakoun</title>
 </head>
 <body>
     
 <h1>formulaire</h1>
 
  <form action="" method="POST">
-    <input type="text" name="prenom" placeholder="prenom">
-    <input type="text" name="nom" placeholder="nom">
-    <input type="text" name="email" placeholder="email">
+    <input type="text" name="prenom" autocomplete="off"  placeholder="prenom">
+    <input type="text" name="nom" autocomplete="off" placeholder="nom">
+    <input type="text" name="email" autocomplete="off" placeholder="email">
     <input type="date" name="ddn">
     <input type="password" name="passwd" placeholder="password">
     <button type="submit" href>ENVOIE LE MOI</button>
@@ -28,9 +28,9 @@ if(!empty($_POST)){
     $passwd = $_POST['passwd'];
 
     $dbPassword="";
-    $pdo = new PDO("mysql:host=localhost;dbname=formulaire" , "root" ,$dbPassword);
+    $pdo = new PDO("mysql:host=localhost;dbname=CRUD", "root", "");
 
-    $requetes = "INSERT INTO information (
+    $requetes = "INSERT INTO eleve (
         prenom,
         nom,
         email,
