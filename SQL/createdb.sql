5917928856ac4de857b251fc9abd786f3b0becea
MatthewSisakoun <matthew.sisakoun@gmail.com>
2024-04-11 06:20:02 +0200

    ajout sql création base + modification formulaire

---
+++ SQL/createdb.sql
@@ -0,0 +1,19 @@
+create database CRUD;
+use CRUD;
+
+CREATE TABLE eleve (
+  `Id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT, -- PK AUTO
+  `Prenom` VARCHAR(50) NOT NULL,
+  `Nom` VARCHAR(100) NOT NULL,
+  `DateNaissance` DATE NOT NULL,
+  `Classe` VARCHAR(45) NULL,
+  `Groupe` CHAR(1) NULL,
+  `Sexe` TINYINT NOT NULL DEFAULT 3,	-- 1 Homme 2 Femme 3 Other
+  `Email` VARCHAR(255) NOT NULL,
+  `Telephone` VARCHAR(20) NULL,
+  `Adresse` VARCHAR(200) NULL,
+  `CodePostal` NUMERIC(5,0) UNSIGNED NULL,
+  `Ville` VARCHAR(100) NULL,
+  `Passwd` VARCHAR(255) NOT NULL,
+   CONSTRAINT UC_EMAIL UNIQUE (`Email`)
+  );
